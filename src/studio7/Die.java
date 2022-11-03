64f08e722f1db98a57c6cb684e9e012ce79b9635
JohnTischke <j.tischke@wustl.edu>
2022-11-03 12:36:52 -0500

    die

--- src/studio7/Die.java
+++ src/studio7/Die.java
@@ -2,4 +2,24 @@ package studio7;
 
 public class Die {
 
+	private int side;
+
+	public Die(int theSide) {
+		this.side = theSide;
+	}
+
+	public int getSide() {
+		return (int) (Math.random() * this.side + 1);
+	}
+
+	@Override
+	public String toString() {
+		return "Die [side=" + side + ", getSide()=" + getSide() + "]";
+	}
+
+	public static void main(String[] args) {
+		Die D1 = new Die(10);
+		System.out.println(D1);
+	}
+
 }
